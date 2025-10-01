b31b3d84f792a9dbfe3e2f69fb6e1c1659b4f8c8
Sachin-1997 <bhatsachin41@gmail.com>
2025-10-01 20:59:46 +0530

    I have a country list using arrayList add method

---
+++ DemoArrayList/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //CRUD = add,get,set, remove
+        ArrayList<String> countryList = new ArrayList<>();
+        countryList.add("India");
+        countryList.add("Russia");
+        countryList.add("Italy");
+        countryList.add("Germany");
+
+        System.out.println(countryList);
+
+    }
+}
